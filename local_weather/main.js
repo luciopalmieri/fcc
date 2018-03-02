a95be01e4c1f377c814c3b0108a67e4b8fb2e527
Lucio Palmieri <lucio.palmieri@gmail.com>
2018-03-02 20:24:38 +0100

    recover local_weather project

--- local_weather/main.js
+++ local_weather/main.js
@@ -1,6 +1,63 @@
+var tempUm = 'C';
+var celsius;
+
 $(document).ready(function() {
-  $('#open-random-wiki').on('click', function (e) {
+  $('#update-weather').on('click', function (e) {
     e.preventDefault();
-    window.open('https://en.wikipedia.org/wiki/Special:Random');
-    });
+    showLocalWeather();
+  });
+
+  $('#temp_um').on('click', function (e) {
+    e.preventDefault();
+    toggleTempUm();
+  });
+
+  showLocalWeather();
 });
+
+function showLocalWeather() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showLocalWeatherByPosition);
+  } else {
+    // "Geolocation is not supported by this browser."
+  }
+
+}
+
+function showLocalWeatherByPosition(position) {
+  $.getJSON('https://fcc-weather-api.glitch.me/api/current?lat=' +position.coords.latitude + '&lon=' + position.coords.longitude, function(data) {
+    console.log(data);
+
+    celsius = data.main.temp;
+
+    var locationText = data.name + ', ' + data.sys.country;
+    var weatherDescription = data.weather[0].description;
+    var weatherStatusIcon = data.weather[0].icon;
+
+    $('#location-text').text(locationText);
+    showTemperature();
+    $('#weather-text').text(weatherDescription);
+    $('#weather-status-icon').attr('src', weatherStatusIcon);
+
+  });
+}
+
+function toggleTempUm() {
+  if ( tempUm === 'C') {
+    tempUm = 'F';
+  } else {
+    tempUm = 'C';
+  }
+  showTemperature();
+}
+
+function showTemperature() {
+  var temp;
+  if ( tempUm === 'C') {
+    temp = celsius;
+  } else {
+    temp = celsius * 1.8 + 32
+  }
+  $('#temperature').text(temp);
+  $('#temp_um').text(tempUm);
+}
--- twitchtv/main.js
+++ twitchtv/main.js
@@ -1,24 +1,31 @@
-var quoteText, quoteAuthor;
+var fcc_channel_ids = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 $(document).ready(function() {
-  $('#new-quote').on('click', function (e) {
-    e.preventDefault();
-    newQuote()
-    });
-
-  $('#tweet-quote').on('click', function(e) {
-    e.preventDefault();
-    window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent("\"" + quoteText + "\" " + quoteAuthor) + '&hashtags=quotes&related=lucio_palmieri');
-  });
-
-  newQuote();
+  showChannels();
 });
 
-function newQuote() {
-  $.getJSON('https://talaikis.com/api/quotes/random/', function(data) {
-    quoteText = data.quote;
-    quoteAuthor = data.author
-    $('#quote-text').text(quoteText);
-    $('#quote-author').text(quoteAuthor);
+function showChannels() {
+  fcc_channel_ids.forEach(function (channel_id) {
+    showChannel(channel_id);
   });
 }
+
+/*
+ * Replace the Twitch API base URL https://api.twitch.tv/kraken with https://wind-bow.gomix.me/twitch-api
+ * (https://wind-bow.glitch.me/)
+ * Note: 'callback=?' parameter convert the call to JSONP which is necessary to make cross-domain calls
+ */
+
+function showChannel(channel_id) {
+  $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/' + channel_id + '?callback=?', function(data) {
+    var channel_data = data;
+    $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + channel_id + '?callback=?', function(data) {
+      var stream_data = data;
+      console.log('CHANNELL ' + channel_id + ": ")
+      console.log(channel_data);
+      console.log('STREAM: ')
+      console.log(stream_data);
+    });
+  });
+}
+
