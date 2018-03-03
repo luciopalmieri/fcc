2d23343301995dec713729b691202178654fb389
Lucio Palmieri <lucio.palmieri@gmail.com>
2018-03-03 21:40:47 +0100

    Migrate Wikipedia viewer to Bootstrap 4

--- wikipedia_viewer/main.js
+++ wikipedia_viewer/main.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
 
-  $('form').submit(function(e) {
-    e.preventDefault();
-    $.getJSON('https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=' + $('#text-to-search').val(), function(data) {
-      $('ul').empty();
-      data[1].forEach(function (value, index) {
-        var title = value;
-        var description = data[2][index];
-        var link = data[3][index];
-        $('ul').append('<li><a href=' + link + ' target="_blank">' + title + '</a></li>' + description);
-      });
-      console.log(data);
-    });
-  });
+  $('#search').click(submit_search);
+  $('form').submit(submit_search);
 
 });
+
+function submit_search(e) {
+  e.preventDefault();
+  $.getJSON('https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=' + $('#text-to-search').val(), function(data) {
+    $('ul').empty();
+    data[1].forEach(function (value, index) {
+      var title = value;
+      var description = data[2][index];
+      var link = data[3][index];
+      $('ul').append('<li><a href=' + link + ' target="_blank">' + title + '</a></li>' + description);
+    });
+    console.log(data);
+  });
+}
