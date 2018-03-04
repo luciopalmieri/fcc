a1f3490eead2d95195e42e920ae98a988f24c506
Lucio Palmieri <lucio.palmieri@gmail.com>
2018-03-04 22:33:20 +0100

    Add list-group to Wikipedia viewer

--- wikipedia_viewer/main.js
+++ wikipedia_viewer/main.js
@@ -8,12 +8,21 @@ $(document).ready(function() {
 function submit_search(e) {
   e.preventDefault();
   $.getJSON('https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=' + $('#text-to-search').val(), function(data) {
-    $('ul').empty();
+    $('#result-list').empty();
     data[1].forEach(function (value, index) {
       var title = value;
       var description = data[2][index];
       var link = data[3][index];
-      $('ul').append('<li><a href=' + link + ' target="_blank">' + title + '</a></li>' + description);
+
+      var listItem =
+        '<a href="' + link + '" target="_blank" class="list-group-item list-group-item-action flex-column align-items-start">\n' +
+        '  <div class=\"d-flex w-100 justify-content-between\">\n' +
+        '    <h5 class=\"mb-1\">' + title + '</h5>\n' +
+        '  </div>\n' +
+        '  <p class=\"mb-1\">' + description + '</p>\n' +
+        '</a>';
+
+      $('#result-list').append(listItem);
     });
     console.log(data);
   });
