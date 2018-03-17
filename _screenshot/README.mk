dbf860bf2dec90beea918eefcf210a9d42bbac47
Lucio Palmieri <lucio.palmieri@gmail.com>
2018-03-17 21:49:26 +0100

    add documentation

--- _screenshot/README.mk
+++ _screenshot/README.mk
@@ -1,5 +1,7 @@
-Install phantomjs and get the web page screenshot with:
+Install [phantomjs](http://phantomjs.org/screen-capture.html) and get the web page screenshot with:
 
 ```sh
 $ phantomjs rasterize.js <url>
 ```
+
+As an alternative use [wkhtmltoimage](https://madalgo.au.dk/~jakobt/wkhtmltoxdoc/wkhtmltoimage_0.10.0_rc2-doc.html)
