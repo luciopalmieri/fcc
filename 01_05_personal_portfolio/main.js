d3afed72ca534dd12c24626af670f845a9a04f17
Lucio Palmieri <lucio.palmieri@gmail.com>
2018-10-27 22:14:31 +0200

    refactoring

--- 01_05_personal_portfolio/main.js
+++ 01_05_personal_portfolio/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("#fullpage").fullpage({
     // Design
-    sectionsColor: ["#763626", "#90AFC5", "#336B87", "#763626"],
+    sectionsColor: ["#763626", "#41637C", "#336B87", "#763626"],
     // Navigation
     anchors: ["s1", "s2", "s3", "s4"],
     menu: "#menu",
--- _screenshot/README.mk
+++ _screenshot/README.mk
@@ -3,5 +3,6 @@ Install [phantomjs](http://phantomjs.org/screen-capture.html) and get the web pa
 ```sh
 $ phantomjs rasterize.js <url>
 ```
+NOTE: phantomjs don't support css var (use sass preprocessor instead)
 
 As an alternative use [wkhtmltoimage](https://madalgo.au.dk/~jakobt/wkhtmltoxdoc/wkhtmltoimage_0.10.0_rc2-doc.html)
